8890915db448d3b472a796b6331614def96483c5
alexandrelima <ale.angeli@hotmail.com>
2020-08-20 10:44:08 -0300

    fix home route

--- src/router/index.js
+++ src/router/index.js
@@ -4,9 +4,20 @@ import Posts from "../pages/Posts";
 import Photos from "../pages/Photos";
 
 export const router = new VueRouter({
-  routes: [
-    { path: "/", name: "home", component: Home },
-    { path: "/posts", name: "posts", component: Posts },
-    { path: "/photos", name: "photos", component: Photos },
+  routes: [{
+      path: "",
+      name: "home",
+      component: Home
+    },
+    {
+      path: "/posts",
+      name: "posts",
+      component: Posts
+    },
+    {
+      path: "/photos",
+      name: "photos",
+      component: Photos
+    },
   ],
 });
